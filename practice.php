158eff6fdf14bd14d26e5e8757718d41494db7f3
mireitanabe <mirei10231023@gmail.com>
2020-07-01 08:39:32 +0000

    laravel 03

--- practice.php
+++ practice.php
@@ -51,10 +51,10 @@ $a = 100;
 $b = 200;
 $c = $a + $b;
 
-echo $c;
+echo $c . "\n" ;
 
 if($name .= "mirei") {
-  echo "私はmireiです";
+  echo "私はmireiです" . "\n" ;
 } else {
   echo "mireiではありません";
 }
@@ -66,17 +66,21 @@ echo $total;
 for ($i = 0; $i <= 10000; $i++){
   $total += $i;
 }
-echo $total;
+echo $total . "\n" ;
 
-/* for文の始めの値を定義する /
+$fruits = array("orang","cherry","peach","pear","fig");
+
+foreach($fruits as $fruits){
+echo  "要素は" . $fruits;
+echo "\n";
+}
+
+
 $start = 1;
-/* for文の終わりの値を定義する */
 $end = 100;
 
-for($i = $start; $i < $end; $i++){
-
-  // 5で割り切れたら{}内を実行する
+for ($i = $start; $i < $end; $i++){
   if($i % 5 == 0){
-    echo $i
-  }
+    echo $i . "\n";
+ }
 }
